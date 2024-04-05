ef7693c438ee9feb987f3428ff3e46060ddbe2aa
doongjun <doongjun.kim@gmail.com>
2024-04-05 10:18:59 +0900

    ex3 - Compose Quadrant

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,9 +14,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -39,89 +42,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-
-    Column {
+    Column(modifier) {
         Row(
-            modifier = Modifier
-                .weight(0.5f)
+            modifier = Modifier.weight(0.5f)
         ) {
-            Row(
-                modifier = Modifier
-                    .weight(0.5f)
+            Card(
+                title = "Text composable",
+                content = "Displays text and follows the recommended Material Design guidelines.",
+                modifier = Modifier.weight(0.5f)
                     .fillMaxHeight()
                     .background(Color(0xFFEADDFF))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = "Text composable",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(text = "Displays text and follows the recommended Material Design guidelines.")
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .weight(0.5f)
+            )
+            Card(
+                title = "Image composable",
+                content = "Creates a composable that lays out and draws a given Painter class object.",
+                modifier = Modifier.weight(0.5f)
                     .fillMaxHeight()
                     .background(Color(0xFFD0BCFF))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = "Image composable",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(text = "Creates a composable that lays out and draws a given Painter class object.")
-                }
-            }
+            )
         }
         Row(
-            modifier = Modifier
-                .weight(0.5f)
+            modifier = Modifier.weight(0.5f)
         ) {
-            Row(
-                modifier = Modifier
-                    .weight(0.5f)
+            Card(
+                title = "Row composable",
+                content = "A layout composable that places its children in a horizontal sequence.",
+                modifier = Modifier.weight(0.5f)
                     .fillMaxHeight()
                     .background(Color(0xFFB69DF8))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = "Row composable",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(text = "A layout composable that places its children in a horizontal sequence.")
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .weight(0.5f)
+            )
+            Card(
+                title = "Column composable",
+                content = "A layout composable that places its children in a vertical sequence.",
+                modifier = Modifier.weight(0.5f)
                     .fillMaxHeight()
                     .background(Color(0xFFF6EDFF))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = "Column composable",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(text = "A layout composable that places its children in a vertical sequence.")
-                }
-            }
+            )
+        }
+    }
+}
+
+@Composable
+fun Card(title: String, content: String, modifier: Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = content,
+                textAlign = TextAlign.Justify,
+            )
         }
     }
 }
