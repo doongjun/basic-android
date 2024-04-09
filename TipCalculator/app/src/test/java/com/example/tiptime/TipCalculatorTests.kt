324d4b2b3cab2fe6a7700bcca6c39e6d4f532a27
doongjun <doongjun.kim@gmail.com>
2024-04-09 17:20:13 +0900

    test: ex3 - TipCalculator

---
+++ TipCalculator/app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,20 @@
+package com.example.tiptime
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+    @Test
+    fun calculateTip_20PercentNoRoundUp() {
+        val amount = 10000.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2000)
+        val actualTip = calculateTip(
+            amount = amount,
+            tipPercent = tipPercent,
+            roundUp = false
+        )
+        assertEquals(expectedTip, actualTip)
+    }
+}
